3d5d45ebe63fca13518b4d18125b0b405163cf36
DCH <1974366335@qq.com>
2021-04-11 20:03:24 +0800

    新增一个学生信息的下拉菜单功能，包括学生ID，学号，姓名，性别，入学年份，年班ID，可直接调用，页面端的内容贴在了notion里

---
+++ src/main/java/com/ruoyi/project/system/common/service/StudentDataForDropdownServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ruoyi.project.system.common.service;
+
+
+import com.ruoyi.project.system.common.domain.StudentDataForDropdown;
+import com.ruoyi.project.system.common.mapper.StudentDataForDropdownMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("stuInfo")
+public class StudentDataForDropdownServiceImpl implements StudentDataForDropdownService{
+
+    @Autowired
+    StudentDataForDropdownMapper studentDataForDropdownMapper;
+
+    @Override
+    public List<StudentDataForDropdown> selectStudentDataForDropdownList(){
+
+        return studentDataForDropdownMapper.selectStudentDataForDropdownByNo();
+
+    }
+}
