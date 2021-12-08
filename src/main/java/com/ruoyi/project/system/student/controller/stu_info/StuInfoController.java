7d994578f749765ecd77ed4750617f09e0182453
langyijun <langyj20@mails.jlu.edu.cn>
2021-12-08 10:04:55 +0800

    delete test.txt and change import package for StuInfoController.java

--- src/main/java/com/ruoyi/project/system/student/controller/stu_info/StuInfoController.java
+++ src/main/java/com/ruoyi/project/system/student/controller/stu_info/StuInfoController.java
@@ -1,24 +1,22 @@
 package com.ruoyi.project.system.student.controller.Stu_Info;
 
 import com.ruoyi.common.utils.security.ShiroUtils;
+import com.ruoyi.framework.aspectj.lang.annotation.Log;
+import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
+import com.ruoyi.framework.web.controller.BaseController;
+import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.project.system.student.domain.PrsnStudent;
+import com.ruoyi.project.system.student.domain.StuInfo;
+import com.ruoyi.project.system.student.service.IPrsnStudentService;
 import com.ruoyi.project.system.user.domain.User;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.ruoyi.framework.aspectj.lang.annotation.Log;
-import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.system.student.domain.PrsnStudent;
-import com.ruoyi.project.system.student.service.IPrsnStudentService;
-import com.ruoyi.framework.web.controller.BaseController;
-import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
-import com.ruoyi.framework.web.page.TableDataInfo;
 
 @Controller
 @RequestMapping("/system/student/stu_info")
